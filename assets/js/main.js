949e87687b5cdc52928dc1d36769a24ca4ba491d
koenidv <32238636+koenidv@users.noreply.github.com>
2018-04-08 21:10:28 +0200

    first attempts on light design and more
    New banner, first room images and email address / phone number updatable from main.js.
    
    The light design is available at the very bottom of the page ("Helles Design (Beta)").

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,75 @@
+lightDesign = false;
+emailaddress = "coderdojo-team@bonifatius-wiesbaden.de"; //This will be shown on every page
+phonenumber = "0611 1575394"; //This will be shown on every page
+
+window.onload = function () {
+	document.getElementById("mailaddress").innerHTML = emailaddress;
+	document.getElementById("phonenumber").innerHTML = phonenumber;
+	document.getElementById("designToggler").innerHTML += " (Beta)";
+	updateDesign();
+}
+
+//Update Design
+function updateDesign() {
+	if (localStorage.getItem("design") == "light") {
+		document.body.style.backgroundColor = "white";
+
+		texts = document.getElementsByClassName("dark");
+		for (var i = 0; i < texts.length; i++) {
+			texts[i].style.color = "#404040";
+		}
+
+		buttons = document.getElementsByClassName("button dark");
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].style.boxShadow = "0 0 0 2px #aaaaaa";
+		}
+
+		specialbuttons = document.getElementsByClassName("special button dark");
+		for (var i = 0; i < specialbuttons.length; i++) {
+			specialbuttons[i].style.backgroundColor = "#404040";
+			specialbuttons[i].style.boxShadow = "0 0 0 2px #404040";
+			specialbuttons[i].style.color = "white";
+		}
+
+		subtexts = document.getElementsByClassName("subdark");
+		for (var i = 0; i < subtexts.length; i++) {
+			subtexts[i].style.color = "#aaaaaa";
+		}
+		//separators = document.getElementsByClassName("separator");
+		//for (var i = 0; i < subtexts.length; i++) {
+		//	separators[i].src = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0+A8AAYUBQcxKiF0AAAAASUVORK5CYII=')";
+		//}
+
+		document.getElementById("headcoderdojo").style.color = "white";
+		document.getElementById("headcoderdojo").style.backgroundColor = "#404040";
+		document.getElementById("headboni").style.color = "#aaaaaa";
+		document.getElementById("menu").style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+		//document.getElementById("menuicon").style.backgroundImage = "url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDUxMiA1MTIiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDUxMiA1MTI7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEyIiBoZWlnaHQ9IjUxMiI + PGc + PGc + Cgk8Zz4KCQk8cGF0aCBkPSJNNDkxLjMxOCwyMzUuMzE4SDIwLjY4MkM5LjI2LDIzNS4zMTgsMCwyNDQuNTc3LDAsMjU2czkuMjYsMjAuNjgyLDIwLjY4MiwyMC42ODJoNDcwLjYzNiAgICBjMTEuNDIzLDAsMjAuNjgyLTkuMjU5LDIwLjY4Mi0yMC42ODJDNTEyLDI0NC41NzgsNTAyLjc0MSwyMzUuMzE4LDQ5MS4zMTgsMjM1LjMxOHoiIGRhdGEtb3JpZ2luYWw9IiMwMDAwMDAiIGNsYXNzPSJhY3RpdmUtcGF0aCIgc3R5bGU9ImZpbGw6IzQwNDA0MCIgZGF0YS1vbGRfY29sb3I9IiMwMDAwMDAiPjwvcGF0aD4KCTwvZz4KPC9nPjxnPgoJPGc + CgkJPHBhdGggZD0iTTQ5MS4zMTgsNzguNDM5SDIwLjY4MkM5LjI2LDc4LjQzOSwwLDg3LjY5OSwwLDk5LjEyMWMwLDExLjQyMiw5LjI2LDIwLjY4MiwyMC42ODIsMjAuNjgyaDQ3MC42MzYgICAgYzExLjQyMywwLDIwLjY4Mi05LjI2LDIwLjY4Mi0yMC42ODJDNTEyLDg3LjY5OSw1MDIuNzQxLDc4LjQzOSw0OTEuMzE4LDc4LjQzOXoiIGRhdGEtb3JpZ2luYWw9IiMwMDAwMDAiIGNsYXNzPSJhY3RpdmUtcGF0aCIgc3R5bGU9ImZpbGw6IzQwNDA0MCIgZGF0YS1vbGRfY29sb3I9IiMwMDAwMDAiPjwvcGF0aD4KCTwvZz4KPC9nPjxnPgoJPGc + CgkJPHBhdGggZD0iTTQ5MS4zMTgsMzkyLjE5N0gyMC42ODJDOS4yNiwzOTIuMTk3LDAsNDAxLjQ1NiwwLDQxMi44NzlzOS4yNiwyMC42ODIsMjAuNjgyLDIwLjY4Mmg0NzAuNjM2ICAgIGMxMS40MjMsMCwyMC42ODItOS4yNTksMjAuNjgyLTIwLjY4MlM1MDIuNzQxLDM5Mi4xOTcsNDkxLjMxOCwzOTIuMTk3eiIgZGF0YS1vcmlnaW5hbD0iIzAwMDAwMCIgY2xhc3M9ImFjdGl2ZS1wYXRoIiBzdHlsZT0iZmlsbDojNDA0MDQwIiBkYXRhLW9sZF9jb2xvcj0iIzAwMDAwMCI + PC9wYXRoPgoJPC9nPgo8L2c + PC9nPiA8L3N2Zz4=')";
+		//document.getElementById("banner").style.backgroundImage = "url('../../banner-light.jpg')";
+		//document.getElementById(banner).style.backgroundColor = "white"; //Needs to be banner:after background-color
+
+		document.getElementById("designToggler").innerHTML = "Dunkles Design";
+	}
+}
+
+
+//Toggle design
+function toggleDesign() {
+	if (localStorage.getItem("design") == "dark") {
+		localStorage.removeItem("design");
+		localStorage.setItem("design", "light");
+		document.getElementById("designToggler").innerHTML = "Dunkles Design";
+		updateDesign();
+	} else {
+		localStorage.removeItem("design");
+		localStorage.setItem("design", "dark");
+		location.reload();
+	}
+}
+
+
+
+
 (function ($) {
 
 	skel.breakpoints({
