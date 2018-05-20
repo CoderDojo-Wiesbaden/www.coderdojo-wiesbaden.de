ac5badf1cc52fe1e5415f6c0ffee18f9045c6b38
koenidv <32238636+koenidv@users.noreply.github.com>
2018-05-20 16:51:32 +0200

    Better readout ui
    Icons instead of text, inline

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,8 +148,8 @@ function starmenu(element) {
 
 /* Readout feature */
 function toggleAudio(file, button) {
-	var play = "Vorlesen",
-		stop = "Stoppen";
+	var playClass = "fa-volume-up",
+		stopClass = "fa-stop";
 
 	var player = document.getElementById("audioPlayer");
 	var source = document.getElementById("audioSource");
@@ -162,12 +162,14 @@ function toggleAudio(file, button) {
 		}
 		//Play and change button text
 		player.play();
-		button.innerHTML = stop;
+		button.classList.remove(playClass);
+		button.classList.add(stopClass);
 	} else {
 		//Pause, Reset time (start at the beginning next time) and change button text
 		player.pause();
 		player.currentTime = 0;
-		button.innerHTML = play;
+		button.classList.remove(stopClass);
+		button.classList.add(playClass);
 		//If the button for another file has been triggered without pausing the last playing file,
 		//change the source, play it and change the text of all buttons (since we dont know what was playing last)
 		if (!source.src.endsWith(file)) {
@@ -175,9 +177,11 @@ function toggleAudio(file, button) {
 			player.load();
 			player.play();
 			Array.prototype.forEach.call(document.getElementsByClassName("readoutbutton"), function (element, index, array) {
-				element.innerHTML = play;
+				element.classList.remove(stopClass);
+				element.classList.add(playClass);
 			});
-			button.innerHTML = stop;
+			button.classList.remove(playClass);
+			button.classList.add(stopClass);
 		}
 	}
 }
