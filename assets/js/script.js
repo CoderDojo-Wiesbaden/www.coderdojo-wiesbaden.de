f557805ec9e18a32d84952dc2ef30b66e5c38a41
koenidv <32238636+koenidv@users.noreply.github.com>
2019-02-24 10:54:11 +0100

    Faster loading, discord redirect

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,20 +50,19 @@ function getNextDojoDate() {
 
 window.onload = function () {
     // For image lazy loading
-	[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-		img.setAttribute('src', img.getAttribute('data-src'));
-		img.onload = function () {
-			img.removeAttribute('data-src');
-		};
+    [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function () {
+            img.removeAttribute('data-src');
+        };
     });
     [].forEach.call(document.querySelectorAll('div[data-bg]'), function (div) {
-        console.log(div)
-		div.style.backgroundImage = "url('" + div.getAttribute('data-bg') + "')";
-		div.onload = function () {
+        div.style.backgroundImage = "url('" + div.getAttribute('data-bg') + "')";
+        div.onload = function () {
             console.log("remove");
-			div.removeAttribute('data-bg');
-		};
-	});
+            div.removeAttribute('data-bg');
+        };
+    });
 }
 
 /* Keyboard shortcuts */
@@ -71,22 +70,37 @@ window.onkeyup = function (e) {
     var key = e.keyCode ? e.keyCode : e.which;
 
     if (key == 27) {
+        /* ESC - toggle menu */
         document.getElementById("menu").classList.toggle("open");
-    } else if (key == 85 || key == 186) {
+    } else if (key == 85 || key == 186 || key == 87) {
+        /* U/Ü/W - About us */
         if (document.getElementById("menu").classList.contains("open")) {
+            document.getElementById("menu").classList.remove("open")
             window.location = "/wir";
         }
     } else if (key == 65 || key == 84) {
+        /* A/T - Signup */
         if (document.getElementById("menu").classList.contains("open")) {
+            document.getElementById("menu").classList.remove("open")
             window.location = "/anmelden";
         }
     } else if (key == 68) {
+        /* D - Direct signup */
         if (document.getElementById("menu").classList.contains("open")) {
+            document.getElementById("menu").classList.remove("open")
             window.location = "https://zen.coderdojo.com/dojos/de/wiesbaden/wiesbaden-at-st-bonifatius";
         }
     } else if (key == 66) {
+        /* B - Blog */
         if (document.getElementById("menu").classList.contains("open")) {
+            document.getElementById("menu").classList.remove("open")
             window.location = "https://blog.cdwi.de";
+        }
+    } else if (key == 67 || key == 83) {
+        /* S/C - Start */
+        if (document.getElementById("menu").classList.contains("open")) {
+            document.getElementById("menu").classList.remove("open")
+            window.location = "/";
         }
     }
 }
